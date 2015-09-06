d904e6af5c9406c30552abced0dd307a41b0f83e
Kevin Doherty <kdoherty11@yahoo.com>
2015-09-06 12:39:21 -0400

    Cleaned up some resources

--- app/src/main/java/com/kdoherty/zipchat/fragments/MessageFavoritesFragment.java
+++ app/src/main/java/com/kdoherty/zipchat/fragments/MessageFavoritesFragment.java
@@ -47,7 +47,7 @@ public class MessageFavoritesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message_details, container, false);
+        return inflater.inflate(R.layout.fragment_message_favorites, container, false);
     }
 
     @Override
